c9d05f1c0ad3b5bfdd59bb88cb76d315fc308c29
asimhamidwo8 <asimhamidwo8@gmail.com>
2026-06-20 06:07:20 +0300

    Fix 1-square.py

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
+"""Module that defines a Square class."""
+
 
 class Square:
+    """Represents a square."""
+
     def __init__(self, size):
+        """Initialize a square."""
         self.__size = size
