9816804328e3fb4d80c0757c7d2940fe95a24d14
asimhamidwo8 <asimhamidwo8@gmail.com>
2026-06-20 23:04:16 +0300

    Fix 0-square.py

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-"""Module that defines an empty Square class."""
+"""Defines an empty Square class."""
 
 
 class Square:
-    """Represents an empty square."""
+    """Empty square class."""
     pass
