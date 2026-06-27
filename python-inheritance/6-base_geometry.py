a442eb397aaf7a7bba98a8d02428ac98e830a6d0
asimhamidwo8 <asimhamidwo8@gmail.com>
2026-06-27 23:00:20 +0300

    Fix missing parenthesis in 6-base_geometry

--- python-inheritance/6-base_geometry.py
+++ python-inheritance/6-base_geometry.py
@@ -7,4 +7,4 @@ class BaseGeometry:
 
     def area(self):
         """Raise an Exception because area is not implemented yet."""
-        raise Exception("area() is not implemented"
+        raise Exception("area() is not implemented")
