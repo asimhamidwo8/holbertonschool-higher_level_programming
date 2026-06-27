28226090b8f1302687b58b6172363782a2dffa9a
asimhamidwo8 <asimhamidwo8@gmail.com>
2026-06-27 22:58:18 +0300

    Add inherits_from (task 4)

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
-"""Module that defines the is_kind_of_class function."""
+"""Module that defines the inherits_from function."""
 
 
-def is_kind_of_class(obj, a_class):
-    """Return True if obj is an instance of a_class or a subclass of it."""
-    return isinstance(obj, a_class)
+def inherits_from(obj, a_class):
+    """Return True if obj is an instance of a class that inherited from
+    a_class (directly or indirectly), but not an instance of a_class itself.
+    """
+    return issubclass(type(obj), a_class) and type(obj) is not a_class
