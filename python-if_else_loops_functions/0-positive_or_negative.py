fb416489ffcc55c5fa8ad336bd52edd2c5fe28c8
asimhamidwo8 <asimhamidwo8@gmail.com>
2026-08-27 09:26:14 +0300

    Fix name 0-positive_or_negative.py

--- python-if_else_loops_functions/0-positive_or_negative.py
+++ python-if_else_loops_functions/0-positive_or_negative.py
@@ -1,3 +1,11 @@
 #!/usr/bin/python3
-for c in range(ord('z'), ord('a') - 1, -1):
-    print("{}".format(chr(c)), end="")
+import random
+
+number = random.randint(-10, 10)
+
+if number > 0:
+    print("{} is positive".format(number))
+elif number < 0:
+    print("{} is negative".format(number))
+else:
+    print("{} is zero".format(number))
