ae1d3126ddec7b69e2f5b0a4debdfdbdc5bf4aa4
asimhamidwo8 <asimhamidwo8@gmail.com>
2026-08-27 09:40:49 +0300

    Fix 6-print_comb3.py

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-for i in range(0, 10):
+
+for i in range(10):
     for j in range(i + 1, 10):
-        for k in range(j + 1, 10):
-            if i == 7 and j == 8 and k == 9:
-                print("{}{}{}".format(i, j, k), end="")
-            else:
-                print("{}{}{}".format(i, j, k), end=", ")
+        if i == 8 and j == 9:
+            print("{}{}".format(i, j))
+        else:
+            print("{}{}".format(i, j), end=", ")
