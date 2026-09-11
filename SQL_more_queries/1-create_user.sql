e81df76dccef9356377e2984bdf6c5b40dfd4a99
asimhamidwo8 <asimhamidwo8@gmail.com>
2026-09-11 22:28:49 +0300

    Fix 1-create_user.sql

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,4 +1,7 @@
--- Create user_0d_1 and grant all privileges
+-- Creates user_0d_1 and grants all privileges
+
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES ON `*`.`*` TO 'user_0d_1'@'localhost';
+
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+
 FLUSH PRIVILEGES;
