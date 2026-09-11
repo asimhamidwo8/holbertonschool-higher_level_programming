4c00542b88eb7bcf00051817ce9d8a8698244fd6
asimhamidwo8 <asimhamidwo8@gmail.com>
2026-09-11 23:13:58 +0300

    Fix 6-states.sql

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -1,5 +1,9 @@
+-- Creates the hbtn_0d_usa database and states table
+
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+
 USE hbtn_0d_usa;
+
 CREATE TABLE IF NOT EXISTS states (
     id INT NOT NULL AUTO_INCREMENT UNIQUE,
     name VARCHAR(256) NOT NULL,
