36b96ced209dab1cd985ce58e4c8bd51bed9847f
asimhamidwo8 <asimhamidwo8@gmail.com>
2026-09-11 23:17:42 +0300

    Fix 7-cities.sql

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -1,10 +1,15 @@
+-- Creates the hbtn_0d_usa database, states table, and cities table
+
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+
 USE hbtn_0d_usa;
+
 CREATE TABLE IF NOT EXISTS states (
     id INT NOT NULL AUTO_INCREMENT UNIQUE,
     name VARCHAR(256) NOT NULL,
     PRIMARY KEY (id)
 );
+
 CREATE TABLE IF NOT EXISTS cities (
     id INT NOT NULL AUTO_INCREMENT UNIQUE,
     state_id INT NOT NULL,
