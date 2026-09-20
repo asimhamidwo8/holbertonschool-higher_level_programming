84794875e65bc11c1ead5b8a653ffc9c93311d77
asimhamidwo8 <asimhamidwo8@gmail.com>
2026-09-20 11:48:24 +0300

    Fix hidden module discovery

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -1,24 +1,10 @@
-#!/usr/bin/env python3
-"""Print names defined in hidden_4.pyc."""
+#!/usr/bin/python3
+"""Print names defined by hidden_4.pyc."""
 
-from xdis import load_module
-
-
-def find_names(code):
-    """Find names recursively in code objects."""
-    names = set(code.co_names)
-
-    for const in code.co_consts:
-        if hasattr(const, "co_names"):
-            names.update(find_names(const))
-
-    return names
+import hidden_4
 
 
 if __name__ == "__main__":
-    result = load_module("/tmp/hidden_4.pyc")
-    code = result[3]
-
-    for name in sorted(find_names(code)):
+    for name in sorted(dir(hidden_4)):
         if not name.startswith("__"):
             print(name)
