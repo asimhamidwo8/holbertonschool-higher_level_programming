c96e1eccd1bb7dec5aa385d889c60c4d5dad6bd4
asimhamidwo8 <asimhamidwo8@gmail.com>
2026-09-22 08:55:48 +0300

    Fix cities filtering by state

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -16,20 +16,19 @@ if __name__ == "__main__":
 
     cursor = db.cursor()
 
-    cursor.execute(
-        """
+    query = """
         SELECT cities.name
         FROM cities
         JOIN states ON cities.state_id = states.id
-        WHERE states.name = %s
+        WHERE BINARY states.name = %s
         ORDER BY cities.id ASC
-        """,
-        (sys.argv[4],)
-    )
+    """
 
-    cities = [city[0] for city in cursor.fetchall()]
-    print(", ".join(cities))
+    cursor.execute(query, (sys.argv[4],))
+
+    rows = cursor.fetchall()
+
+    print(", ".join(row[0] for row in rows))
 
     cursor.close()
     db.close()
-
